f0ca6aedcbca7c01f0780b00af899329a92bd262
guinux <nuxgui@gmail.com>
2011-11-12 19:51:58 +0100

    Add french translation

--- files/lang/French/mod_captcha_registration.php
+++ files/lang/French/mod_captcha_registration.php
@@ -3,7 +3,7 @@
 
 $lang_mod_captcha_registration = array(
   'Captcha title'      => 'Code de vérification',
-  'Captcha info'       => 'Pour éviter les inscriptions automatiques, vous devez entrez un code de vérification. Si vous ne pouvez pas lire l\'image cliquez %sici%s pour rafraichir.',
+  'Captcha info'       => 'Pour éviter les inscriptions automatiques, vous devez entrer un code de vérification. Si vous ne pouvez pas lire l\'image cliquez %sici%s pour rafraichir.',
   'Captcha question'   => 'Code de vérification',
   'Captcha fail'       => 'Le code de vérification que vous avez entré est incorrect!',
 );
