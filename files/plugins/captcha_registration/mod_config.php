a17c936691cef08c444d4b003c2a9a8c51e2efa9
Roland Singer <roland@manjarolinux.org>
2012-02-25 09:17:43 +0300

    update to fluxbb 1.4.8

--- files/plugins/captcha_registration/mod_config.php
+++ files/plugins/captcha_registration/mod_config.php
@@ -2,14 +2,15 @@
 
 $mod_config = array (
   'mod_name' => 'Captcha Registration',
-  'version' => '1.1.1',
-  'release_date' => '2011-12-05',
+  'version' => '1.2.0',
+  'release_date' => '2012-02-25',
   'author' => 'Roland Singer - Manjaro Linux',
   'mod_installer' => 'OK',
   'fluxbb_versions' => 
   array (
     0 => '1.4.6',
     1 => '1.4.7',
+	2 => '1.4.8',
   ),
   'date_install' => 0,
   'mod_status' => 0,
